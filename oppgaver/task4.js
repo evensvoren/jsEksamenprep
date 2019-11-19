53245fb6b692ced430b91e5b7dd0b081cc54db36
Even Svoren <32481279+evensvoren@users.noreply.github.com>
2019-11-19 14:23:23 +0100

    Add files via upload

--- oppgaver/task4.js
+++ oppgaver/task4.js
@@ -1,14 +1,34 @@
 
-function startup(){
+
+function plusKnapp(){
     var numbfelt1 = document.getElementById("input1").value;
     var numbfelt2 = document.getElementById("input2").value;
-   var plus = document.getElementById("plus").value;
-    document.getElementById("minus").value;
-    document.getElementById("gange").value;
-    document.getElementById("dele").value;
+    var integer1 = parseInt(numbfelt1, 10);
+    var integer2 = parseInt(numbfelt2, 10);
+    var resplus = integer1 + integer2;
+    document.getElementById("svaret").innerHTML = resplus;
+}
 
-    if(plus === true){
-        var res = numbfelt1 + numbfelt2;
-        document.getElementById("svaret").innerHTML = res;
-    }
+function minusKnapp(){
+    var numbfelt1 = document.getElementById("input1").value;
+    var numbfelt2 = document.getElementById("input2").value;
+
+    var resmin = numbfelt1 - numbfelt2;
+    document.getElementById("svaret").innerHTML = resmin;
+}
+
+function gangeKnapp(){
+    var numbfelt1 = document.getElementById("input1").value;
+    var numbfelt2 = document.getElementById("input2").value;
+
+    var resgang = numbfelt1 * numbfelt2;
+    document.getElementById("svaret").innerHTML = resgang;
+}
+
+function deleKnapp(){
+    var numbfelt1 = document.getElementById("input1").value;
+    var numbfelt2 = document.getElementById("input2").value;
+
+    var resdel = numbfelt1 / numbfelt2;
+    document.getElementById("svaret").innerHTML = resdel;
 }
